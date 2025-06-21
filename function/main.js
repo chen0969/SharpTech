f72b2858a99184135b647855de56a71c2b26122c
Canada-Broccoli <chen0969@algonquinlive.com>
2025-06-21 15:57:45 -0400

    push

---
+++ function/main.js
@@ -0,0 +1,5 @@
+    const openIg = () => {
+      window.open('https://www.instagram.com/sharpfle_ca?utm_source=ig_web_button_share_sheet&igsh=MW8ydTBobzdma21jMw==', '_blank');
+    };
+
+    document.getElementById('logo').addEventListener('click', openIg);
